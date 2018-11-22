cbcc1168ea5184e50be4325215ab87f563e963b7
扶冬冬 <fudongdong@meituan.com>
2018-11-22 20:56:39 +0800

    创建FSViewToImage工程

---
+++ FSViewToImage/FSViewToImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FSViewToImage
+//
+//  Created by fudongdong on 2018/11/22.
+//  Copyright © 2018年 fudongdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FSViewToImage/FSViewToImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FSViewToImage
+//
+//  Created by fudongdong on 2018/11/22.
+//  Copyright © 2018年 fudongdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
