5b862765435e6a328ec8cc49a36a125651598773
fudon <fuart@qq.com>
2018-11-22 22:48:43 +0800

    创建文件

---
+++ FSViewToImage/Classes/FSViewToImage.h
@@ -0,0 +1,20 @@
+//
+//  FSViewToImage.h
+//  FSViewToImage
+//
+//  Created by FudonFuchina on 2018/11/22.
+//  Copyright © 2018年 fudongdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FSViewToImage : NSObject
+
++ (UIImage *)imageForUIView:(UIView*)view;
++ (UIImage *)captureScrollView:(UIScrollView *)scrollView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FSViewToImage/Classes/FSViewToImage.m
@@ -0,0 +1,45 @@
+//
+//  FSViewToImage.m
+//  FSViewToImage
+//
+//  Created by FudonFuchina on 2018/11/22.
+//  Copyright © 2018年 fudongdong. All rights reserved.
+//
+
+#import "FSViewToImage.h"
+
+@implementation FSViewToImage
+
++ (UIImage*)imageForUIView:(UIView*)view{
+    //    UIGraphicsBeginImageContext(view.bounds.size);// 只会生成屏幕所见的部分
+    CGSize size = view.bounds.size;
+    if ([view isKindOfClass:UIScrollView.class]) {
+        UIScrollView *sView = (UIScrollView *)view;
+        size = CGSizeMake(sView.frame.size.width,sView.contentSize.height+ sView.contentInset.top+ sView.contentInset.bottom);
+    }
+    UIGraphicsBeginImageContextWithOptions(size, YES, view.layer.contentsScale);
+    CGContextRef currnetContext = UIGraphicsGetCurrentContext();
+    [view.layer renderInContext:currnetContext];
+    //    CGContextRestoreGState(currnetContext);
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
++ (UIImage *)captureScrollView:(UIScrollView *)scrollView{
+    CGRect frame = scrollView.frame;
+    //设置控件显示的区域大小     key:显示
+    scrollView.frame = CGRectMake(0, scrollView.frame.origin.y, scrollView.contentSize.width, scrollView.contentSize.height);
+    
+    //设置截屏大小(截屏区域的大小必须要跟视图控件的大小一样)
+    UIGraphicsBeginImageContextWithOptions(scrollView.frame.size, YES, 0.0);
+    [[scrollView layer] renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *viewImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    scrollView.frame = frame;
+    return viewImage;
+}
+
+@end
