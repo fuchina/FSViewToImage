4cb7a6659528e8a4892f8d9db44be18a3a262f1f
扶冬冬 <fudongdong@meituan.com>
2018-11-27 15:07:08 +0800

    优化屏幕截图方法

--- FSViewToImage/Classes/FSViewToImage.m
+++ FSViewToImage/Classes/FSViewToImage.m
@@ -13,25 +13,62 @@
 + (UIImage *)imageForUIView:(nonnull UIView *)view{
     CGSize size = view.bounds.size;
     CGRect savedFrame = view.frame;
-    if ([view isKindOfClass:UIScrollView.class]) {
+    BOOL isScrollView = [view isKindOfClass:UIScrollView.class];
+    if (isScrollView) {
         UIScrollView *sView = (UIScrollView *)view;
-        size = CGSizeMake(sView.frame.size.width,sView.contentSize.height+ sView.contentInset.top + sView.contentInset.bottom);
+        size = CGSizeMake(sView.frame.size.width,sView.contentSize.height + sView.contentInset.top + sView.contentInset.bottom);
         view.frame = CGRectMake(view.frame.origin.x, view.frame.origin.y, size.width, size.height);
     }
-    UIGraphicsBeginImageContextWithOptions(size, YES, 0.0);
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0);
     CGContextRef currnetContext = UIGraphicsGetCurrentContext();
     [view.layer renderInContext:currnetContext];
     
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     
-    view.frame = savedFrame;
+    if (isScrollView) {
+        view.frame = savedFrame;
+    }
     return image;
 }
 
 + (UIImage *)screenShot{
-    UIView *view = UIApplication.sharedApplication.delegate.window;
-    return [self imageForUIView:view];
+    UIApplication *app = [UIApplication sharedApplication];
+    UIView *statusBar = [app valueForKey:@"_statusBar"];
+    NSAssert(statusBar != nil, @"获取状态栏失败");
+    UIImage *image1 = [self imageForUIView:statusBar];
+    UIImage *image2 = [self screenShotApple];
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height)];
+    imageView.image = image2;
+    UIImageView *sbImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, statusBar.frame.size.width, statusBar.frame.size.height)];
+    sbImageView.image = image1;
+    [imageView addSubview:sbImageView];
+    return [self imageForUIView:imageView];
+}
+
++ (UIImage *)screenShotApple{
+    CGSize imageSize = [[UIScreen mainScreen] bounds].size;
+    UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    for (UIWindow *window in [[UIApplication sharedApplication] windows]) {
+        if (![window respondsToSelector:@selector(screen)] || [window screen] == [UIScreen mainScreen]) {
+            CGContextSaveGState(context);
+            CGContextTranslateCTM(context, [window center].x, [window center].y);
+            CGContextConcatCTM(context, [window transform]);
+            CGContextTranslateCTM(context,
+                                  -[window bounds].size.width * [[window layer] anchorPoint].x,
+                                  -[window bounds].size.height * [[window layer] anchorPoint].y);
+            
+            [[window layer] renderInContext:context];
+            CGContextRestoreGState(context);
+        }
+    }
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
 }
 
 @end
