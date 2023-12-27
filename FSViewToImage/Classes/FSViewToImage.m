668d6f6c6e26371ea380770361115575e9567baf
fudongdong <fudongdong@ihuman.com>
2023-12-27 15:01:10 +0800

    Update FSViewToImage.m

--- FSViewToImage/Classes/FSViewToImage.m
+++ FSViewToImage/Classes/FSViewToImage.m
@@ -12,7 +12,7 @@
 @implementation FSViewToImage
 
 + (UIImage *)imageForUIView:(nonnull UIView *)view {
-    return [FSUIKit imageForUIView:view];
+    return [FSImage imageForUIView:view];
 }
 
 + (UIImage *)screenShot {
